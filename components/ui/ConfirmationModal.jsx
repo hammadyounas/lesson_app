42d73a15d71d605e682dd97e0c047d8a1e7d6c87
Hammad Younus <m.hammadyounas@outlook.com>
2024-10-30 19:57:47 +0500

    minor changes

--- components/ui/ConfirmationModal.jsx
+++ components/ui/ConfirmationModal.jsx
@@ -4,8 +4,8 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm">
-      <div className="bg-gray-800 px-6 py-8 rounded shadow-md w-[25%]">
+    <div className="fixed inset-0 flex items-center justify-center max-h-screen bg-opacity-50 backdrop-blur-sm">
+      <div className="bg-gray-800 px-6 py-8 rounded shadow-md xl:w-[25%] sm:w-[50%] w-[90%]">
         <h2 className=" font-semibold text-white">Are you sure you want to delete?</h2>
         <div className="mt-6 flex justify-end space-x-2">
           <button className="px-4 py-2 bg-gray-300 text-black rounded" onClick={onClose}>
